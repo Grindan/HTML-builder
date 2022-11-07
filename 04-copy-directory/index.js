fdd65c3e622d7ad7e8210cdf6affea9530ec14b0
Grindan <polina.koroleva@gmail.com>
2022-11-08 02:24:22 +0300

    feat: update tasks to make them reusable

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,28 @@
 const path = require('path');
 const fs = require('fs/promises');
 
-const copy = async () => {
-    const sourcePath = path.join(__dirname, 'files');
-    const destinationPath = path.join(__dirname, 'files_copy');
+const copy = async (sourceFolderPath, destinationFolderPath) => {
+    await fs.rm(destinationFolderPath, { recursive: true, force: true });
+    await fs.mkdir(destinationFolderPath, { recursive: true });
 
-    await fs.rm(destinationPath, { recursive: true, force: true });
-    await fs.mkdir(destinationPath);
-
-    const files = await fs.readdir(sourcePath);
+    const files = await fs.readdir(sourceFolderPath, { withFileTypes: true });
     for (const file of files) {
-        await fs.copyFile(`${sourcePath}/${file}`, `${destinationPath}/${file}`);
+        if (file.isFile()) {
+            await fs.copyFile(`${sourceFolderPath}/${file.name}`, `${destinationFolderPath}/${file.name}`);
+        } else {
+            const newFolder = path.join(destinationFolderPath, file.name);
+            await mkdir(newFolder, { recursive: true });
+            await copy(`${sourceFolder}/${file.name}`, `${destinationFolder}/${file.name}`);
+        }
     }
 
     console.log('✅ Successfully copied.');
 };
 
-copy();
+((sourceFolder, destinationFolder) => {
+    const sourcePath = path.join(__dirname, sourceFolder);
+    const destinationPath = path.join(__dirname, destinationFolder);
+    copy(sourcePath, destinationPath);
+})('files', 'files_copy');
+
+module.exports = { copy };
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,28 +1,32 @@
 const path = require('path');
 const fs = require('fs/promises');
 
-const merge = async () => {
-    const sourcePath = path.join(__dirname, 'styles');
-    const destinationPath = path.join(__dirname, 'project-dist', 'bundle.css');
-
-    await fs.rm(destinationPath, { recursive: true, force: true });
+const merge = async (sourceFolderPath, destinationFolderPath) => {
+    await fs.rm(destinationFolderPath, { recursive: true, force: true });
     let result = '';
 
-    const contents = await fs.readdir(sourcePath, { withFileTypes: true });
+    const contents = await fs.readdir(sourceFolderPath, { withFileTypes: true });
     for (const item of contents) {
         const isFile = item.isFile();
         const isStyleFile = path.extname(item.name) === '.css';
         const shouldBeParsed = isFile && isStyleFile;
 
         if (shouldBeParsed) {
-            const filePath = path.join(sourcePath, item.name);
+            const filePath = path.join(sourceFolderPath, item.name);
             const data = await fs.readFile(filePath, { encoding: 'utf-8' });
             result += `${data}\n`;
         }
     }
-    await fs.writeFile(destinationPath, result);
+
+    await fs.writeFile(destinationFolderPath, result, { flag: 'wx' });
 
     console.log('✅ Successfully merged.');
 };
 
-merge();
+((sourceFolder, destinationFolder) => {
+    const sourcePath = path.join(__dirname, sourceFolder);
+    const destinationPath = path.join(__dirname, destinationFolder);
+    merge(sourcePath, destinationPath);
+})('styles', 'project-dist/bundle.css');
+
+module.exports = { merge };

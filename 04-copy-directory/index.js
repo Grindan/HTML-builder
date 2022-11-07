aaef7533f994051f7e2a89e3d1e1aa5958a07619
Grindan <polina.koroleva@gmail.com>
2022-11-08 01:02:43 +0300

    feat: implement files copying

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+const copy = async () => {
+    const sourcePath = path.join(__dirname, 'files');
+    const destinationPath = path.join(__dirname, 'files_copy');
+
+    await fs.rm(destinationPath, { recursive: true, force: true });
+    await fs.mkdir(destinationPath);
+
+    const files = await fs.readdir(sourcePath);
+    for (const file of files) {
+        await fs.copyFile(`${sourcePath}/${file}`, `${destinationPath}/${file}`);
+    }
+
+    console.log('✅ Successfully copied.');
+};
+
+copy();
